20d85a9e7439f2a86b77d6d8d9d84a4567357059
s309677 <s309677@studenti.polito.it>
2026-01-15 19:31:35 +0100

    quest/mods/config: removed silent gear, SFM and some other minor (redundant) mods. Transitioned to using EMI instead of JEI. Now the pack ships with included defaultoptions. Small updates to the quests to include mid-game goals rewards

---
+++ kubejs/server_scripts/pack/removed_recipes.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(ternion => { 
+    ternion.remove({ id: 'enchantinginfuser:advanced_enchanting_infuser'})
+    ternion.remove({ id: 'enchantinginfuser:enchanting_infuser'})
+})
---
+++ kubejs/server_scripts/unification/gear.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(ternion => { 
+    ternion.remove({ id: 'industrialforegoing:iron_gear'})
+    ternion.remove({ id: 'industrialforegoing:gold_gear'})
+    ternion.remove({ id: 'industrialforegoing:diamond_gear'})
+})
